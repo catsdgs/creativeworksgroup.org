fba04b876a8ca6cb248ff139fb856a87170030e7
catsdgs <therealcatsdgs@gmail.com>
2018-02-09 20:15:59 -0700

    Update form.php

--- form.php
+++ form.php
@@ -6,29 +6,26 @@
     $captcha_verify = open_https_url("https://www.google.com/recaptcha/api/siteverify?secret=$6LdvhUUUAAAAAF1wEgnuzYuhLQ98nvYYSwSMlMp-t&response=$client_captcha_response&remoteip=$user_ip");
     $captcha_verify_decoded = json_decode($captcha_verify);
     if(!$captcha_verify_decoded->success)
-      die('DIRTY ROBOT');
+      die('Captcha Not Completed');
 
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
     $from = 'From: My Website';
     $to = 'therealcatsdgs@gmail.com';
-    $subject = 'Request Form';
+    $subject = 'Contact Form';
 
     $body = "Name: $name \n E-Mail: $email \nMessage:\n$message";
 
     if ($_POST['submit']) {
         if ($email != '') {
-            if ($human == '4') {                 
+            {                 
                 if (mail ($to, $subject, $body, $from)) { 
                     echo '<p>You have successfully submitted your information.</p>';
                 } else { 
                     echo '<p>Something went wrong, go back and try again!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>'; 
                 } 
-            } else if ($_POST['submit'] && $human != '4') {
-                echo '<p>You answered the anti-spam question incorrectly!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>';
-            }
         } else {
             echo '<p>You need to fill in all required fields!!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>';
         }
-    }
+
