4a9c0029cd68fb5a3e166a39f0408d2ef98e59e6
catsdgs <therealcatsdgs@gmail.com>
2018-02-09 20:26:20 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,31 +1,42 @@
 <?php
-    $your_secret = 6LdvhUUUAAAAAF1wEgnuzYuhLQ98nvYYSwSMlMp-";
-    $client_captcha_response = $_POST['g-recaptcha-response'];
-    $user_ip = $_SERVER['REMOTE_ADDR'];
-
-    $captcha_verify = open_https_url("https://www.google.com/recaptcha/api/siteverify?secret=$6LdvhUUUAAAAAF1wEgnuzYuhLQ98nvYYSwSMlMp-t&response=$client_captcha_response&remoteip=$user_ip");
-    $captcha_verify_decoded = json_decode($captcha_verify);
-    if(!$captcha_verify_decoded->success)
-      die('Captcha Not Completed');
-
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $from = 'From: My Website';
-    $to = 'therealcatsdgs@gmail.com';
-    $subject = 'Contact Form';
-
-    $body = "Name: $name \n E-Mail: $email \nMessage:\n$message";
-
-    if ($_POST['submit']) {
-        if ($email != '') {
-            {                 
-                if (mail ($to, $subject, $body, $from)) { 
-                    echo '<p>You have successfully submitted your information.</p>';
-                } else { 
-                    echo '<p>Something went wrong, go back and try again!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>'; 
-                } 
-        } else {
-            echo '<p>You need to fill in all required fields!!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>';
-        }
-
+if(isset($_POST['submit']) && !empty($_POST['submit'])):
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
+        //your site secret key
+        $secret = '6LdvhUUUAAAAAGfcqknpX0WbWyVgbzRFPp-pn4ct';
+        //get verify response data
+        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success):
+            //contact form submission code
+            $name = !empty($_POST['name'])?$_POST['name']:'';
+            $email = !empty($_POST['email'])?$_POST['email']:'';
+            $message = !empty($_POST['message'])?$_POST['message']:'';
+            
+            $to = 'therealcatsdgs@gmail.com.com';
+            $subject = 'New contact form have been submitted';
+            $htmlContent = "
+                <h1>Contact request details</h1>
+                <p><b>Name: </b>".$name."</p>
+                <p><b>Email: </b>".$email."</p>
+                <p><b>Message: </b>".$message."</p>
+            ";
+            // Always set content-type when sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            // More headers
+            $headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
+            //send email
+            @mail($to,$subject,$htmlContent,$headers);
+            
+            $succMsg = 'Your contact request have submitted successfully.';
+        else:
+            $errMsg = 'Robot verification failed, please try again.';
+        endif;
+    else:
+        $errMsg = 'Please click on the reCAPTCHA box.';
+    endif;
+else:
+    $errMsg = '';
+    $succMsg = '';
+endif;
+?>
