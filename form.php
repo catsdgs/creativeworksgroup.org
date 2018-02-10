29d630b7f5f7c85a52bc4481cdd92d1df3689334
catsdgs <therealcatsdgs@gmail.com>
2018-02-09 20:12:57 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,54 +1,34 @@
 <?php
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, [
-    'secret' => $privatekey,
-    'response' => $_POST['g-recaptcha-response'],
-    'remoteip' => $_SERVER['REMOTE_ADDR']
-]);
+    $your_secret = 6LdvhUUUAAAAAF1wEgnuzYuhLQ98nvYYSwSMlMp-";
+    $client_captcha_response = $_POST['g-recaptcha-response'];
+    $user_ip = $_SERVER['REMOTE_ADDR'];
 
-$resp = json_decode(curl_exec($ch));
-curl_close($ch);
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$from = 'Demo Contact Form'; 
-		$to = 'therealcatsdgs@gmail.com'; 
-		$subject = 'Message from Contact Demo ';
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
+    $captcha_verify = open_https_url("https://www.google.com/recaptcha/api/siteverify?secret=$6LdvhUUUAAAAAF1wEgnuzYuhLQ98nvYYSwSMlMp-t&response=$client_captcha_response&remoteip=$user_ip");
+    $captcha_verify_decoded = json_decode($captcha_verify);
+    if(!$captcha_verify_decoded->success)
+      die('DIRTY ROBOT');
 
-                if ($resp->success) {
-                 } else {
-                $errMessage = 'Please complete the captcha';
-                }
-		}
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errReCaptcha) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
-}
-	}
-?>
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: My Website';
+    $to = 'therealcatsdgs@gmail.com';
+    $subject = 'Request Form';
+
+    $body = "Name: $name \n E-Mail: $email \nMessage:\n$message";
+
+    if ($_POST['submit']) {
+        if ($email != '') {
+            if ($human == '4') {                 
+                if (mail ($to, $subject, $body, $from)) { 
+                    echo '<p>You have successfully submitted your information.</p>';
+                } else { 
+                    echo '<p>Something went wrong, go back and try again!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>'; 
+                } 
+            } else if ($_POST['submit'] && $human != '4') {
+                echo '<p>You answered the anti-spam question incorrectly!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>';
+            }
+        } else {
+            echo '<p>You need to fill in all required fields!!</p><p><input type="button" value="Go Back" onclick="history.back(-1)" class="goback" /></p>';
+        }
+    }
